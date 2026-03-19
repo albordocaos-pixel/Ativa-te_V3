a7345b0e3148e5d1a023f6245ceed9beb325659d
albordocaos-pixel <albordocaos@gmail.com>
2026-03-19 13:31:31 +0100

    v0.4

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,10 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <div style={{ color: 'black', padding: '20px' }}>
+    <h1>Hello World</h1>
+    <p>If you see this, the app is working.</p>
+  </div>
 );
 
 export default App;
